f2cdbd263eb9247b477653218c4ba292bcc49c40
Glitch (angry-slayer) <none>
2019-04-09 11:56:43 +0000

    👨‍❤️‍👨🍤 Updated with Glitch

--- db_init.js
+++ db_init.js
@@ -7,22 +7,56 @@ var knex = require('knex')({
     useNullAsDefault: true
 });
 
-async function init()
+async function init_dropUsers()
 {
+   var drop_users = await knex.schema.dropTableIfExists('users');
+};
 
-   await knex.schema.dropTableIfExists('users')
-      .then(() => {
-      knex.schema.createTable('users', (table) => {
-        table.string('login',255).primary();
-        table.string('pass',255).notNull();
-        table.string('name',255);
-        table.integer('color1',10).defaultTo(0);
-        table.integer('color2',10).defaultTo(0);
-        table.string('token');
-      })
+
+async function init_tableUsers()
+{
+  
+ var create_table = await knex.schema.createTable('users', function (table) {
+  table.string('login',255).primary();
+  table.string('pass', 255).notNullable();
+  table.string('name',255);
+  table.string('color1',10);
+  table.string('color2',10);
 });
- await knex.destroy();
+   
+};
 
+async function info_column()
+{
+  var info_col = await knex('users').columnInfo();
+};
+
+async function ajout()
+{
+  
+   var add_jojo = await knex.raw('INSERT INTO users VALUES (?, ?, ?, ?, ?)',
+               [ 'Sword', 'jojo', 'Joran', 'green', 'black' ]);
+    
+  var add_maxou = await knex.raw('INSERT INTO users VALUES (?, ?, ?, ?, ?)',
+               [ 'Maxou', 'max', 'Maxime', 'red', 'yellow' ]);
+ 
+  
+};
+
+async function selection()   
+{
+  var aff = await knex('users');
+  console.log(aff);
+  
+  var destroy = await knex.destroy();
+};
+
+async function go() {
+  await init_dropUsers()
+  await init_tableUsers()
+  await ajout()
+  await info_column()
+  await selection()
 }
 
-
+go()
--- server.js
+++ server.js
@@ -1,19 +1,63 @@
 // server.js
 // where your node app starts
-
+var knex = require('knex')({
+    client: 'sqlite3',
+    connection: {
+        filename: ".data/db.sqlite3"
+    },
+    debug: true,
+    useNullAsDefault: true
+});
 // init project
+
 const express = require('express');
 const app = express();
+const bodyP = require('body-parser');
+const cookieP = require('cookie-parser');
+require('express-async-errors');
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app
+    .use(bodyP.urlencoded({ extended: false }))
+    .use(cookieP())
+    .use('/s', express.static('public'));
+
+const consolidate = require('consolidate');
+app.engine('html', consolidate.nunjucks);
+app.set('view engine', 'nunjucks');
+app.set('views', './views');
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get('/signin',async function(req, res) {
+  res.sendFile(__dirname + '/views/signin.html');
+});
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.post('/signin',async function(req, res) {
+  var add_jojo = await knex.raw('INSERT INTO users VALUES (?, ?, ?, ?, ?)',
+               [ req.body.login, req.body.pass, req.body.name, req.body.fav, req.body.sec]);
+ res.redirect('/signin');
+});
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get('/logout', function(req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get('/userlist', async function(req, res) {
+  let aff = await knex('users').select('login','name','color1');
+  console.log(aff);
+  res.render('userlist.html',{'liste': aff})
+
 });
 
 // listen for requests :)
